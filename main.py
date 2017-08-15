e7f35957d7ce605e7ba958dd39bb3cc18926a0c8
João Trevizoli Esteves <joao@lab804.com.br>
2017-08-14 23:47:59 -0300

    testes

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+# https://github.com/naokishibuya/car-finding-lane-lines
 import cv2
 import numpy as np
 
 img = cv2.imread('DJI_0065.jpg', 0)
+
 kernel = np.ones((3, 3), np.uint8)
-print(kernel)
+
 erosion = cv2.erode(img, kernel, iterations=1)
 
-kernel2 = np.ones((2, 2), np.uint8)
+cv2.namedWindow('erosion', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('erosion', 600,600)
+cv2.imshow('erosion', erosion)
 
-dilation = cv2.dilate(erosion, kernel2, iterations=2)
+k = cv2.waitKey()
+
+kernel2 = np.ones((3, 3), np.uint8)
+
+dilation = cv2.dilate(erosion, kernel2, iterations=1)
 
 cv2.namedWindow('dilation', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('dilation', 600,600)
@@ -16,6 +24,9 @@ cv2.resizeWindow('dilation', 600,600)
 cv2.imshow('dilation', dilation)
 
 # cv2.imshow("teste", img)
-k = cv2.waitKey(0)
+k = cv2.waitKey()
+
+
+
 
 cv2.destroyAllWindows()
