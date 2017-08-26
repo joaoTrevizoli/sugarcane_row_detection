f7e199b5ab605f8724c40cbbd2d9cdfe56a3f9bd
João Trevizoli Esteves <joao@lab804.com.br>
2017-08-26 02:01:13 -0300

    docstring

--- main.py
+++ main.py
@@ -1,131 +1,20 @@
-# https://github.com/naokishibuya/car-finding-lane-lines
-import cv2
-import numpy as np
-
 
 if __name__ == '__main__':
     from sugarcane_line_finder import *
+    import re
 
-    sugarCaneImages = SugarCanePreProcessing('cana_01.jpg', "cana.jpg", True)
-    pre_processed_image = sugarCaneImages()
-    final = SugarCaneLineFinder(pre_processed_image, "pos_cana.jpg", True)
-    final('cana_01.jpg')
+    b_path = base_path()
+    images = os.listdir("{}/{}".format(b_path, "base_images"))
+    allowed_extensions = ("jpg", "JPG", "png", "img", "gif", "bmp")
+    pattern = re.compile("(cana)")
+    image_path = ["{}/base_images/{}".format(b_path, i) for i in images if
+                  i.split(".")[1] in allowed_extensions and pattern.search(i)]
+    image_names = [i for i in images if i.split(".")[1] in allowed_extensions and pattern.search(i)]
 
-    # sugarCaneImages.save(sugarCaneImages.gaussian_smooth)
+    sugarcane_images = [{"img": cv2.imread(i[0]), "name": i[1]} for i in zip(image_path, image_names)]
 
-    # sugarCaneImages.show("plain")
-    #
-    # image = cv2.imread('caca_01.jpg')
-    #
-    # # show(image, 'plain image')
-    #
-    # green_mask = select_rgb_green(image)
-    #
-    #
-    # # show(green_mask, 'green filter')
-    #
-    # cv2.imwrite("green_filter.jpg", green_mask)
-    #
-    # erode_callback = cv2.erode
-    #
-    # erosion = morphological_transformation(green_mask, 1, 1, erode_callback)
-    # # show(erosion, 'erosion')
-    #
-    # cv2.imwrite("erosion.jpg", erosion)
-    #
-    # dilation = morphological_transformation(erosion, 3, 1, cv2.dilate)
-    # # show(dilation, 'dilation')
-    #
-    # cv2.imwrite("dilation.jpg", dilation)
-    #
-    #
-    # smoothed_image = gaussian_smooth(dilation)
-    # # show(smoothed_image, "Gaussian Smooth")
-    #
-    # cv2.imwrite("smoothed_image.jpg", smoothed_image)
-    #
-    #
-    # edges = canny(smoothed_image)
-    # # show(edges, 'edge detection')
-    #
-    # cv2.imwrite("edges.jpg", edges)
-    #
-    # im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # cv2.drawContours(image, contours, -1, (0,255,0), 3)
-    #
-    # print(len(contours))
-    #
-    # # show(image, "countours")
-    # cv2.imwrite("countours.jpg", image)
-    #
-    #
-    # # length = len(contours)
-    # # status = np.zeros((length, 1))
-    # #
-    # # for i, cnt1 in enumerate(contours):
-    # #     x = 1
-    # #     if i != length-1:
-    # #         for j, cnt2 in enumerate(contours[i+1:]):
-    # #             x = x + 1
-    # #             dist = find_close(cnt1, cnt2)
-    # #             if dist is True:
-    # #                 val = min(status[i], status[x])
-    # #                 status[x] = status[i] = val
-    # #             else:
-    # #                 try:
-    # #                     if status[x] == status[i]:
-    # #                         status[x] = i+1
-    # #                 except Exception as e:
-    # #                     print(e)
-    # # unified = []
-    # # maximum = int(status.max()) + 1
-    # # for i in range(maximum):
-    # #     pos = np.where(status == i)[0]
-    # #     if pos.size != 0:
-    # #         cont = np.vstack(contours[i] for i in pos)
-    # #         hull = cv2.convexHull(cont)
-    # #         unified.append(hull)
-    #
-    # # cv2.drawContours(image, unified, -1, (0,255,0), 2)
-    # # show(image, "countours")
-    # cv2.imwrite("countours.jpg", image)
-    #
-    #
-    #
-    #
-    #
-    # # lines = hough_lines(edges)
-    # # for i in lines:
-    # #     x1, y1, x2, y2 = i[0]
-    # #
-    # #     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 1)
-    #
-    # # print(lines)
-    # # teste = []
-    # # for i in lines:
-    # #     x1, y1, x2, y2 = i[0]
-    # #     teste.append([x1, y1])
-    # #     teste.append([x2, y2])
-    #
-    # # print(teste)
-    # # teste = (np.sort(teste, axis=1))
-    # # teste = np.int32([teste])
-    # # cv2.polylines(image, np.array(teste), 3, (0, 255, 0))
-    #
-    # # show(image, "lines")
-    #
-    #
-    # c = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # # show(cv2.drawContours(image, c[1], 1, (0, 255, 0), 3), 'teste')
-    #
-    # # for x1, y1, x2, y2 in lines:
-    # #     cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0) ,2)
-    # #     show(image, "lines")
-    # # print(gradient_orientation(edges))
-    # # teste = (general_hough_closure(edges))
-    # # fct = general_hough_closure(edges, image)
-    #
-    # # test_general_hough(general_hough_closure, image)
-
-    cv2.destroyAllWindows()
-
+    pre_processors = SugarCanePreProcessing.multiple_processor(sugarcane_images, True)
+    for i, j in zip(pre_processors, sugarcane_images):
+        pre_processed = i()
+        image = j["img"].copy()
+        SugarCaneLineFinder(pre_processed, j["name"], True)(image)
--- sugarcane_line_finder.py
+++ sugarcane_line_finder.py
@@ -1,3 +1,14 @@
+"""""Sugar Cane line finder
+
+Copyright 2017, Lab804
+
+.. module: sugarcane_line_finder
+   :platform: Unix, Windows, macOS
+   :synopsis: An openCV application for crop rows detection
+
+.. moduleauthor:: João Trevizoli Esteves <joao@lab804.com.br>
+"""
+
 import cv2
 import os
 import tabulate
@@ -7,10 +18,41 @@ from functools import wraps
 
 
 def base_path():
+    """Gets base file path
+
+    Returns
+    -------
+    :param base_path:
+        str with this file path.
+    :rtype: str
+    """
     return os.path.dirname(os.path.abspath(__file__))
 
 
 def save(func):
+    """Image saving decorator.
+
+    Decorator for wrapping openCV functionalities and save them
+    at the folder output_images
+
+    Parameters
+    ----------
+    :param func:
+        Function pointer.
+    :type: func
+
+    Returns
+    -------
+    :returns image_wrapper:
+        Function pointer returns
+    :rtype: numpy array
+
+    Notes
+    -----
+    The image names are going to be renamed based on the wrapped method or
+    function
+
+    """
     __path = "{}/{}/".format(base_path(), "output_images")
 
     @wraps(func)
@@ -25,7 +67,22 @@ def save(func):
 
 
 class SugarCaneProcessingBase(object):
+    """Processors base class
 
+    Parameters
+    ----------
+    :param base_image:
+        OpenCV base image file name or numpy array
+    :type: str or numpy array
+
+    :param name:
+        Output file name
+    :type: str
+
+    :param save_mode:
+        Image logging, default=False
+    :type: bool
+    """
     __input_path = "{}/{}/".format(base_path(), "base_images")
     __output_path = "{}/{}/".format(base_path(), "output_images")
 
@@ -41,15 +98,25 @@ class SugarCaneProcessingBase(object):
         self.name = name
         self.save_mode = save_mode
 
-    @classmethod
-    def multiple_processor(cls, iterator, save_mode=False):
-        for image in iterator:
-            if isinstance(image["img"], str):
-                img_path = cls.__input_path + image[0]
-                image["img"] = cv2.imread(img_path, cv2.IMREAD_COLOR)
-            yield cls(image["img"], image["name"], save_mode)
-
     def opencv_show(self, img=None, name=None, kill_all=True):
+        """Display image through openCV.
+
+        Display images using openCV resources
+
+        Parameters
+        ----------
+        :param img:
+            opened openCV image, default=None
+        :type: numpy array
+
+        :param kill_all:
+            Kill all windows on click
+        :type: bool
+
+        :param name:
+            Window name
+        :type: str
+        """
         if img is None:
             img = self.base_image
             name = self.name
@@ -61,6 +128,17 @@ class SugarCaneProcessingBase(object):
             cv2.destroyAllWindows()
     
     def matplotlib_show(self, img=None):
+        """Display image through matplotlib.
+
+        Display images using matplotlib resources
+
+        Parameters
+        ----------
+        :param img:
+            opened openCV image
+        :type: numpy array
+
+        """
         if img is None:
             img = self.base_image
         plt.imshow(img)
@@ -68,16 +146,90 @@ class SugarCaneProcessingBase(object):
 
 
 class SugarCanePreProcessing(SugarCaneProcessingBase):
+    """Crop images pre-processor.
+
+    Pre-processor for crop images
+
+    """
+    @classmethod
+    def multiple_processor(cls, iterator, save_mode=False):
+        """Class method to receive iterators.
+
+        classmethod for receiving and dealing with iterators.
+
+        Parameters
+        ----------
+        :param iterator:
+            iterator with dict in the form:
+                {"img": numpy array,
+                 "name": str
+                 }
+        :type: list, tuple or iterator
+
+        :param save_mode:
+            Image logging, default=False
+            :type: bool
+
+        Yield
+        -------
+        :yield: cls
+
+        .. warning:: The dict style should be used, otherwise
+                     this method will not work
+        """
+        for image in iterator:
+            if isinstance(image["img"], str):
+                img_path = cls.__input_path + image[0]
+                image["img"] = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            yield cls(image["img"], image["name"], save_mode)
 
     @save
     def select_rgb_green(self):
+        """ RGB mask.
+
+        Method used to segregate the green spectrum
+
+        Return
+        -------
+        :return: mask
+        :type: numpy array
+
+        """
         lower = np.uint8([65, 190, 0])
         upper = np.uint8([178, 255, 175])
         mask = cv2.inRange(self.base_image, lower, upper)
         return mask
 
     @save
-    def morphological_transformation(self, img, kernel_shp, iterations, fct):
+    def morphological_transformation(self, img, kernel_shp, iterations, func):
+        """ Morphological transformation wrapper.
+
+        wraps all openCV morphological transformations
+
+        Parameters
+        ----------
+        :param img:
+            opened openCV image, default=None
+        :type: numpy array
+
+        :param kernel_shp:
+            Square matrix shape
+        :type: int or tuple
+
+        :param iterations:
+            number of iterations
+        :type: int
+
+        :param func:
+            openCV morphological operator function
+        :type: method or function
+
+        Return
+        -------
+        :return transformed_img
+        :type: numpy array
+
+        """
         if not isinstance(kernel_shp, tuple) and not isinstance(kernel_shp, int):
             raise TypeError("shape must be int or tuple")
 
@@ -86,22 +238,64 @@ class SugarCanePreProcessing(SugarCaneProcessingBase):
 
         kernel = np.ones(kernel_shp, np.uint8)
 
-        transformed_img = fct(img, kernel=kernel, iterations=iterations)
+        transformed_img = func(img, kernel=kernel, iterations=iterations)
         return transformed_img
 
     @save
     def to_gray_scale(self, img=None):
+        """Convert image to gray scale.
+
+        Parameters
+        ----------
+        :param img:
+            opened openCV image, default=None
+        :type: numpy array
+
+        Return
+        -------
+        :return transformed_img
+        :type: numpy array
+        """
         if img is None:
             img = self.base_image
         return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
     @save
     def gaussian_smooth(self, img=None, kernel_shape=15):
+        """Apply Gaussian smooth
+
+        Apply the gaussian smooth to an image
+
+        Parameters
+        ----------
+        :param img:
+            opened openCV image, default=None
+        :type: numpy array
+
+        :param kernel_shp:
+            Square matrix shape
+        :type: int or tuple
+
+        Return
+        -------
+        :return blured image
+        :type: numpy array
+        """
         if img is None:
             img = self.base_image
         return cv2.GaussianBlur(img, (kernel_shape, kernel_shape), 0)
 
     def __call__(self):
+        """Call in correct order
+
+        Call all the methods in the correct order and
+        return smoothed image
+
+        Return
+        -------
+        :return blured image
+        :type: numpy array
+        """
         green_mask = self.select_rgb_green()
         erosion = self.morphological_transformation(green_mask, 1, 1, cv2.erode)
         dilation = self.morphological_transformation(erosion, 3, 1, cv2.dilate)
@@ -109,17 +303,48 @@ class SugarCanePreProcessing(SugarCaneProcessingBase):
 
 
 class SugarCaneLineFinder(SugarCaneProcessingBase):
+    """Sugar Cane processor
 
+    Finds the crop rows into an image
+    """
     def __init__(self, gauss_filtered_image, name, save_mode=False):
         self.__plants = 0
         SugarCaneProcessingBase.__init__(self, gauss_filtered_image, name, save_mode)
 
     @save
     def canny(self, low_thress=0, high_thress=10):
+        """ Edge detector
+
+        Apply the Canny algorithm for edge detection
+
+        Parameters
+        ----------
+        :param low_thress:
+        :type: int
+
+        :param high_thress:
+        :type: int
+
+        Return
+        -------
+        :return: bitmap with draw edges
+        :rtype: numpy array
+        """
         return cv2.Canny(self.base_image, low_thress, high_thress)
 
     @save
     def get_lines(self, output_image):
+        """Find all the contours
+
+        Find all the contours by applying the Suzuki's topological structural 
+        analysis of digitized binary images by border following algorithm
+
+        Parameters
+        ----------
+        :param output_image:
+            image to receive the contours
+        :type: numpy array
+        """
         __input_path = "{}/{}/".format(os.path.dirname(os.path.abspath(__file__)), "base_images")
         if isinstance(output_image, str):
             img_path = __input_path + output_image
@@ -128,17 +353,35 @@ class SugarCaneLineFinder(SugarCaneProcessingBase):
         img = self.canny()
         _, contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.__plants = len(contours)
-        cv2.drawContours(output_image, contours, -1, (0,255,0), 3)
+        cv2.drawContours(output_image, contours, - 1, (0,255,0), 3)
         return output_image
 
     def __call__(self, output_image):
+        """Call methods in order
+        
+        Call all the methods in the correct order
+        
+        Parameters
+        ----------
+        :param output_image:
+            image to receive the contours
+ 
+        """
         return self.get_lines(output_image)
 
     def __str__(self):
+        """Print table
+        
+        Print a table with an report
+        
+        :return: table
+        :rtype: str
+        """
         img = self.canny()
         _, contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        body = [["Image Shape", self.base_image.shape, "numpy array"],
-                ["Image size", self.base_image.size, "pixels"],
+        body = [["Image Resolution", self.base_image.shape[0:2], "numpy array"],
+                ["Chanels", 1, "int"],
+                ["Image size", self.base_image.size/8192, "kb"],
                 ["Sugarcane clumps", len(contours), "int"]]
         headers = ["Feature", "Report", "Type"]
         table = tabulate.tabulate(body,
